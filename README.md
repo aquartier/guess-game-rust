abf4b7a486cd70053f8a24eb9723a6320d2835fa
Phureewat Aphibansri <a.phureewat@gmail.com>
2021-02-07 23:37:55 +0700

    (feat): guess game in Rust

---
+++ README.md
@@ -0,0 +1,11 @@
+# Guess Game
+
+The first hands-on Rust project with a classic programming problem: The Guessing Game. Focusing on learning the simple pattern matching with Rust.
+
+## How it works?
+
+It will generate random number between 1 and 100. It will prompt to input a guess number. Then the program will indicate whether the guess was `too low` or `too high` along with the guess attempts left. If the guess is correct or it ran out of the attempts, the game will over.
+
+## Run
+
+Run the `cargo run` command to start the game.
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use rand::Rng;
+use std::io::{self, Write};
+use std::cmp::Ordering;
+
+const MAX_NUMBER: i32 = 100;
+const MAX_ATTEMPTS: i32 = 5;
+
+enum State {
+    TooHigh,
+    TooLow,
+    Won,
+    Lost
+}
+
+struct GuessGame {
+    secret_number: i32,
+    attempts: i32,
+}
+
+impl GuessGame {
+    fn guess(&self, number: i32) -> State {
+        if self.attempts == MAX_ATTEMPTS {
+            return State::Lost;
+        }
+        match number.cmp(&self.secret_number) {
+            Ordering::Less => State::TooLow,
+            Ordering::Greater => State::TooHigh,
+            Ordering::Equal => State::Won,
+        }
+    }
+}
+
+fn main() {
+    let mut game = GuessGame {
+        secret_number: rand::thread_rng().gen_range(0, MAX_NUMBER),
+        attempts: 0,
+    };
+    println!("Welcome to Guess Game! Let guess a secret number between 0 to {:?}.", MAX_NUMBER);
+
+    loop {
+        print!("Attempts {:?} of {:?}: ", game.attempts, MAX_ATTEMPTS);
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+
+        let number: i32 = input.trim().parse().expect("Input is not a number");
+        game.attempts += 1;
+
+        match game.guess(number) {
+            State::TooLow => println!("Your guess was too low."),
+            State::TooHigh => println!("Your guess was too high."),
+            State::Won => {
+                println!("You win!");
+                break;
+            },
+            State::Lost => {
+                println!("You lost. The secret number was: {:?}", game.secret_number);
+                break;
+            },
+        }
+    }
+}
